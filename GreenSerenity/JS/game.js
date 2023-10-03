1162b090a1c7fb001ee585c8c7f4fa08310b4390
Aaron Dangc <adraging@gmail.com>
2023-10-02 22:34:18 -0700

    made an inventory system need make ui to display it for game

--- GreenSerenity/JS/game.js
+++ GreenSerenity/JS/game.js
@@ -1,3 +1,4 @@
+
 // Make sure game.js is loaded in index.html
 console.log("game.js loaded");
 
@@ -5,7 +6,7 @@ console.log("game.js loaded");
 // --------------------------------------------------CODE BREAKDOWN------------------------------------------------------
 // -----------------------------------------------------------------------------------------------------------------------
 
-// ConfigureScene - preload assets and create the trash objects which all level scenes will use
+// ConfigureScene - preload assets which other scenes will use and start the menu scene
 // Menu - create the menu scene
 // Level1 - create the first level scene
 // Level2 - create the second level scene
@@ -16,15 +17,22 @@ console.log("game.js loaded");
 // --------------------------------------------------CONFIGURE SCENE------------------------------------------------------
 // -----------------------------------------------------------------------------------------------------------------------
 
+
 class ConfigureScene extends Phaser.Scene {
     constructor(scenekey) {
         super(scenekey);
+        // font properties
         this.fontproperties = {font:'Pixelify Sans'};
+        // inventory array
+        this.inventory = [];
+        this.itemcount = 0;
+        // flag for recognizing speech
+        this.recognitionInProgress = false;
     }
     preload() {
         // WHERE WE PRELOAD ALL ASSETS
         // Load the font
-    this.load.script('webfont', 'https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js');
+        this.load.script('webfont', 'https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js');
 
     }
     create(){
@@ -38,6 +46,11 @@ class ConfigureScene extends Phaser.Scene {
             },
         });
 
+    }
+
+    collectItem(item){
+        this.inventory.push(item);
+        this.itemcount++;
     }
 }
 
@@ -58,7 +71,7 @@ class Menu extends ConfigureScene {
             fontSize: 30,
         },);
 
-        const startButton = this.add.text(20, 100, 'Say "start" to begin the game!', {
+        const startButton = this.add.text(20, 100, 'Say "start" to begin the game or click here!', {
             fontFamily: this.fontproperties.font,
             fontSize: 30,
         },);
@@ -71,22 +84,28 @@ class Menu extends ConfigureScene {
         console.log("recognizer started");
 
         recognizer.onresult = (event) => {
-            for(let i = event.resultIndex; i < event.results.length; i++){
-                const transcript = event.results[i][0].transcript.toLowerCase();
-                console.log(transcript); 
-
-                if (transcript.includes("start") && !isGameStarted) {
-                    isGameStarted = true;
-                    recognizer.stop();
-                    console.log("recognizer stopped");
-                    this.scene.start('firstLevel');
-                }
-            }   
+            if (!this.recognitionInProgress) {
+                for(let i = event.resultIndex; i < event.results.length; i++){
+                    const transcript = event.results[i][0].transcript.toLowerCase();
+                    console.log(transcript); 
+                    if (transcript.includes("start") && !isGameStarted) {
+                        // recognition is in progress
+                        this.recognitionInProgress = true;
+                        // start the game and set isGameStarted to true and stop the recognizer
+                        isGameStarted = true;
+                        recognizer.stop();
+                        console.log("recognizer stopped");
+                        this.scene.start('firstLevel');
+                    }
+                }   
+            }
         }
-        
-
-       
-
+        // start the game when the start button is clicked instead of saying "start" as another option
+        startButton.setInteractive();
+        startButton.on('pointerdown', () => {
+            recognizer.stop();
+            this.scene.start('firstLevel');
+        });
     }
 
 }
@@ -105,9 +124,83 @@ class firstLevel extends ConfigureScene {
             fontSize: 30,
         },);
 
+        const item1 = this.add.text(20, 100, 'Say "item" to collect the item or click here!', {
+            fontFamily: this.fontproperties.font,
+            fontSize: 30,
+        },);
+
+        let recognizer = new webkitSpeechRecognition();
+        recognizer.continuous = true;
+        recognizer.interimResults = true;
+        recognizer.lang = "en-US";
+        recognizer.start();
+        console.log("recognizer started");
+
+        recognizer.onresult = (event) => {
+            if (!this.recognitionInProgress) {
+                for(let i = event.resultIndex; i < event.results.length; i++){
+                    const transcript = event.results[i][0].transcript.toLowerCase();
+                    console.log(transcript);
+    
+                    if (transcript.includes("item")) {
+                        // recognition is in progress stops it and collect the item and stop the recognizer
+                        this.recognitionInProgress = true;
+                        this.collectItem(item1);
+                        recognizer.stop();
+                        item1.destroy();
+                        console.log("recognizer stopped");
+                        console.log("inventory",this.inventory);
+                        console.log(this.itemcount);  
+                    }
+                }
+            }
+            
+        }
+
+
+
     }
 
 }
+
+
+// ---------------------------------------------------------------------------
+// -----------------------------Level2 Scene------------------------------------
+// ---------------------------------------------------------------------------
+
+class secondLevel extends ConfigureScene {
+    constructor(){
+        super('secondLevel');
+    }
+    create(){
+        this.add.text(20,20, "Level2 Scene", {
+            fontFamily: this.fontproperties.font,
+            fontSize: 30,
+        },);
+
+    }
+
+}
+
+
+// ---------------------------------------------------------------------------
+// -----------------------------Level3 Scene------------------------------------
+// ---------------------------------------------------------------------------
+
+class thirdLevel extends ConfigureScene {
+    constructor(){
+        super('thirdLevel');
+    }
+    create(){
+        this.add.text(20,20, "Level3 Scene", {
+            fontFamily: this.fontproperties.font,
+            fontSize: 30,
+        },);
+
+    }
+
+}
+
 
 
 
